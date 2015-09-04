38950b569435eab9f6792d4c56b8e1acbf47aee3
Cristobal Ocampo Quintero <cocampo5@eafit.edu.co>
2015-09-03 22:39:05 -0500

    Se implementan otras 2 funciones
    Se implementó seno y coseno, tangente parece no servir

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,5 +1,10 @@
 #include "stdafx.h"
 
+/*
+Para las operaciones básicas utilizamos conjunto de instrucciones SSE y
+registros xmmm
+*/
+
 void suma(){ //Recibe floatantes de 32 bits
 	float i, j,res;
 	cout << "Ingrese el primer numero: ";
@@ -13,7 +18,7 @@ void suma(){ //Recibe floatantes de 32 bits
 	};
 	cout << "Su resultado es: " << res << endl;
 }
-void resta(){ //Recibe floatantes de 32 bits
+void resta(){ //Recibe flotantes de 32 bits
 	float i, j, res;
 	cout << "Ingrese el primer numero: ";
 	cin >> i;
@@ -26,7 +31,7 @@ void resta(){ //Recibe floatantes de 32 bits
 	};
 	cout << "Su resultado es: " << res << endl;
 }
-void multiplicacion(){ //Recibe floatantes de 32 bits
+void multiplicacion(){ //Recibe flotantes de 32 bits
 	float i, j, res;
 	cout << "Ingrese el primer numero: ";
 	cin >> i;
@@ -39,7 +44,7 @@ void multiplicacion(){ //Recibe floatantes de 32 bits
 	};
 	cout << "Su resultado es: " << res << endl;
 }
-void division(){ //Recibe floatantes de 32 bits
+void division(){ //Recibe flotantes de 32 bits
 	float i, j, res;
 	cout << "Ingrese el primer numero: ";
 	cin >> i;
@@ -52,10 +57,58 @@ void division(){ //Recibe floatantes de 32 bits
 	};
 	cout << "Su resultado es: " << res << endl;
 }
+/*
+Trigonometricas
+Todo se maneja con la FPU (Floating Point Unit)
+FLD: Esto carga un 32 bits a st[0], puedo pasarle de 32,64,80 bits
+FSIN/FCOS/FPTAN: Hace el seno/coseno/tangete-parcial del almacenado en st[0]
+y lo almacena en el mismo st[0]
+FST: Almacena en una variable el st[0]
+*/
+void seno(){
+	float i,res;
+	float constante = 0.0174532925f; //Para pasar de grados a radianes
+	cout << "Ingrese angulo en grados : ";
+	cin >> i;
+	i = i*constante;//grados a radianes
+	__asm{
+		
+		FLD [i] 
+		FSIN 
+		FST [res]
+	}
+	cout << "Su resultado es: " << res << endl;
+}
+void coseno(){
+	float i, res;
+	float constante = 0.0174532925f;
+	cout << "Ingrese angulo en grados : ";
+	cin >> i;
+	i = i*constante;
+	__asm{
+		FLD[i]
+		FCOS
+		FST[res]
+	}
+	cout << "Su resultado es: " << res << endl;
+}
+void tangente(){
+	float i, res;
+	float constante = 0.0174532925f;
+	cout << "Ingrese angulo en grados : ";
+	cin >> i;
+	i = i*constante;
+	__asm{
+		FLD[i]
+		FPTAN
+		FST[res]
+	}
+	cout << "Su resultado es: " << res << endl;
+}
 int main(int argc, char* argv[])
 {
 	int elect;
-	cout << "Hola, tiene 13 operaciones:\n1->Suma 2->Resta 3->Multipliacion 4->Division\n";
+	cout << "Hola, tiene 13 operaciones:\n1->Suma 2->Resta 3->Multipliacion 4->Division\n5->Seno 6->Coseno 7->Tangente\n";
 	cout << "Ingrese su seleccion: "; //Son 13 opciones 
 	cin >> elect;
 	switch (elect)
@@ -73,8 +126,14 @@ int main(int argc, char* argv[])
 			division();
 			break;
 		case 5:
+			seno();
+			break;
 		case 6:
+			coseno();
+			break;
 		case 7:
+			tangente();
+			break;
 		case 8:
 		case 9:
 		case 10:
